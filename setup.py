0db3e67fe84a6685ad174c82843b5d1b6bf629b6
Piotr <pplonski86@gmail.com>
2024-08-27 16:28:44 +0200

    set version 0.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="supertree",
-    version="0.0.8",
+    version="0.1.0",
     description="Visualize decision tree in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
